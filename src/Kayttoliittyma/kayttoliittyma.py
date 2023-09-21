c43dd54ff18689433d6aef598756ab016383915b
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-09-21 12:36:04 +0300

    käyttöliittymä päivitetty. Funktioiden testaaminen siirretty automaattitesteihin

--- src/Kayttoliittyma/kayttoliittyma.py
+++ src/Kayttoliittyma/kayttoliittyma.py
@@ -1,27 +1,22 @@
 import Funktiot.alkulukutesti as primalitytest
 import Funktiot.alkulukugeneraattori as generator
+import time
 
 
 def UI():
     while True:
         print("Mitä tehdään?")
-        options = {1 : "Testaa alkuluku",
-                2 : "Jaa kahden potenssit",
-                3 : "Tuota n-bittinen alkuluku",
+        options = {1 : "Tuota n-bittinen alkuluku",
                 0 : "Lopeta"}
         for key, value in options.items():
             print(key, ":", value)
         choise = int(input())
         if choise == 1:
-            target = int(input("Mitä lukua testataan? "))
-            attempts = int(input("Monta kertaa haastetaan? "))
-            print(primalitytest.probable_prime_test(target,attempts))
-        if choise == 2:
-            target = int(input("Mitä lukua tutkitaan? "))
-            print(primalitytest.factor_out(target))
-        if choise == 3:
             bits = int(input("Montako bittiä? "))
             attempts = int(input("Monta kertaa haastetaan? "))
+            start = time.time()
             print(generator.generate_probable_prime(bits,attempts))
+            end = time.time()
+            print(f"Aikaa kului {end-start}s.")
         if choise == 0:
             break
