fcd0af37534553b197de2bb5821536dad6efbabb
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-09-21 12:54:46 +0300

    vaihdettu pythonin valmiiseen modulaariseen potenssiin

--- src/Funktiot/alkulukutesti.py
+++ src/Funktiot/alkulukutesti.py
@@ -8,11 +8,14 @@ def probable_prime_test(target: int, attempts: int, first_primes: list):
         if target%i == 0:
             return "Ei alkuluku, jaollinen pienellä alkuluvulla"
     (s, d)=factor_out(target-1)
+    past_attempts = []
     for i in range(attempts):
-        a = random.randint(557,target-2)
-        x = a%target
-        for ii in range(1,d):
-            x = (x*a)%target
+        while True:
+            a = random.randint(557,target-2)
+            if a not in past_attempts:
+                break
+        past_attempts.append(a)
+        x = pow(a,d,target)
         for iii in range (s):
             y = ((x%target)*x)%target
             if y == 1 and x != 1 and x != target-1:
