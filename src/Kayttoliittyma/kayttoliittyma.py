229889a25e41fbc8d8ae57e1fc8a70d9d981c393
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-09-28 16:16:18 +0300

    avainten testaus lisätty käyttöliittymään

--- src/Kayttoliittyma/kayttoliittyma.py
+++ src/Kayttoliittyma/kayttoliittyma.py
@@ -1,16 +1,21 @@
-import Funktiot.alkulukugeneraattori as generator
+import Funktiot.alkulukugeneraattori as primegenerator
+import Funktiot.avaingeneraattori as keygenerator
 
 def UI():
     while True:
         print("Mitä tehdään?")
         options = {1 : "Tuota n-bittinen alkuluku",
-                0 : "Lopeta"}
+                   2 : "Tuota avainpari",
+                   0 : "Lopeta"}
         for key, value in options.items():
             print(key, ":", value)
         choise = int(input())
         if choise == 1:
             bits = int(input("Montako bittiä? "))
             attempts = int(input("Monta kertaa haastetaan? "))
-            print(generator.generate_probable_prime(bits,attempts))
+            print(primegenerator.generate_probable_prime(bits,attempts))
+        if choise == 2:
+            (public_modulus, public_exponent, private_decryptor) = keygenerator.generate_key()
+            print(f"Julkinen avain ({public_modulus}, {public_exponent}) ja yksityinen avain {private_decryptor}")
         if choise == 0:
             break
