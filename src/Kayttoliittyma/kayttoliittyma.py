c6436d929b67cff1bff3e20df796404f77a56ef1
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-10-06 15:52:15 +0300

    ei vielä toimiva salaus/purku

--- src/Kayttoliittyma/kayttoliittyma.py
+++ src/Kayttoliittyma/kayttoliittyma.py
@@ -1,24 +1,40 @@
 import Funktiot.alkulukugeneraattori as primegenerator
 import Funktiot.avaingeneraattori as keygenerator
-import Funktiot.avaintallennus as save
+import Funktiot.avaintallennus as keyhandler
+import Funktiot.salaus as encrypt
 
 def UI():
     while True:
         print("Mitä tehdään?")
         options = {1 : "Tuota n-bittinen alkuluku",
                    2 : "Tuota avainpari",
+                   3 : "Salaa viesti",
+                   4 : "Pura viesti",
                    0 : "Lopeta"}
         for key, value in options.items():
             print(key, ":", value)
-        choise = int(input())
-        if choise == 1:
+        choise = input()
+        if choise == "1":
             bits = int(input("Montako bittiä? "))
             attempts = int(input("Monta kertaa haastetaan? "))
             print(primegenerator._generate_probable_prime(bits,attempts))
-        if choise == 2:
+        if choise == "2":
             (public_modulus, public_exponent, private_decryptor) = keygenerator._generate_key()
             print(f"Julkinen avain ({public_modulus}, {public_exponent}) ja yksityinen avain {private_decryptor}")
             name = input("Millä nimellä avain tallennetaan? ")
-            save._save_key(name, public_modulus,public_exponent,private_decryptor)
-        if choise == 0:
+            keyhandler._save_key(name, public_modulus,public_exponent,private_decryptor)
+        if choise == "3":
+            msg_to_encrypt = input("Salattava viesti: ")
+            key_to_use = input("Millä avaimella: ")
+            public_key = keyhandler._get_key(key_to_use+"_public")
+            encrypt._encrypt(msg_to_encrypt, public_key)
+        if choise =="4":
+            file_to_decrypt = input("Minkä nimisen tiedoston viestin salaus puretaan? ")
+            key_to_use = input("Millä avaimella: ")
+            private_key = keyhandler._get_key(key_to_use+"_public")
+            encrypt._decrypt(file_to_decrypt,private_key)
+        if choise =="5":
+            string =input()
+            encrypt.testing(string)
+        if choise == "0":
             break
