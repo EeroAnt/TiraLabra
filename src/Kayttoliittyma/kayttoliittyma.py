415668f9b63bd76273fb7a638aa0669eed1a14da
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-10-19 13:25:32 +0300

    käyttöliittymää hiottu valmiimman ohjelman suuntaiseksi

--- src/Kayttoliittyma/kayttoliittyma.py
+++ src/Kayttoliittyma/kayttoliittyma.py
@@ -6,24 +6,22 @@ import Funktiot.salaus as encrypt
 def UI():
     while True:
         print("Mitä tehdään?")
-        options = {1 : "Tuota n-bittinen alkuluku",
-                   2 : "Tuota avainpari",
-                   3 : "Salaa viesti",
-                   4 : "Pura viesti",
+        options = {1 : "Tuota avainpari",
+                   2 : "Salaa viesti",
+                   3 : "Pura viesti",
                    0 : "Lopeta"}
         for key, value in options.items():
             print(key, ":", value)
         choise = input()
         if choise == "1":
-            bits = int(input("Montako bittiä? "))
-            attempts = int(input("Monta kertaa haastetaan? "))
-            print(primegenerator._generate_probable_prime(bits,attempts))
-        if choise == "2":
             (public_modulus, public_exponent, private_decryptor) = keygenerator._generate_key()
-            print(f"Julkinen avain ({public_modulus}, {public_exponent}) ja yksityinen avain {private_decryptor}")
             name = input("Millä nimellä avain tallennetaan? ")
-            keyhandler._save_key(name, public_modulus,public_exponent,private_decryptor)
-        if choise == "3":
+            try:
+                keyhandler._save_key(name, public_modulus,public_exponent,private_decryptor)
+                print(f"Avain, {name}, tallennettu")
+            except:
+                print("Jotain meni pieleen")
+        if choise == "2":
             msg_to_encrypt = input("Salattava viesti: ")
             key_to_use = input("Millä avaimella: ")
             public_key = keyhandler._get_key(key_to_use+"_public")
@@ -31,7 +29,7 @@ def UI():
             if public_key != None:
                 encrypt._encrypt(msg_to_encrypt, public_key, name_of_message)
                 print("Salattu")
-        if choise =="4":
+        if choise =="3":
             file_to_decrypt = input("Minkä nimisen tiedoston viestin salaus puretaan? ")
             key_to_use = input("Millä avaimella: ")
             private_key = keyhandler._get_key(key_to_use+"_private")
